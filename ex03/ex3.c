e83f770bef6b3fbed40c408a9c7f12e60419ba65
Zachary Spector <zacharyspector@gmail.com>
2014-12-28 11:29:48 -0500

    break it #2

--- ex03/ex3.c
+++ ex03/ex3.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-  int age = 10;
+  int age;
   int height = 72;
 
-  printf("I am %d years old.\n");
+  printf("I am %d years old.\n", age);
   printf("I am %d inches tall.\n", height);
 
   return 0;
