23295011872f7dd6b7035d0b5969958cd1402c01
Zachary Spector <zacharyspector@gmail.com>
2014-12-28 11:29:06 -0500

    break it #1 redux

--- ex03/ex3.c
+++ ex03/ex3.c
@@ -2,9 +2,10 @@
 
 int main()
 {
+  int age = 10;
   int height = 72;
 
-  printf("I am %d years old.\n", age);
+  printf("I am %d years old.\n");
   printf("I am %d inches tall.\n", height);
 
   return 0;
